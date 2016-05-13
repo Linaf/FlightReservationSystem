b86bcf7850d0e7244c7f8e0a6fed1ea60e84a06d
985041 <985041@CS_Lab115-301.cs.mum.edu>
2016-05-13 12:18:53 -0500

    initial commit

---
+++ flightreservation/src/main/resources/domain/Flight.java
@@ -0,0 +1,13 @@
+
+public class Flight {
+	
+	private int id;
+	private string flightCode;
+	private String from;
+	private String to;
+	private LocalDate departureDate;
+	private LocalDate arrivalDate;
+	private LocalDate retdepartureDate;
+	private LocalDate retarrivalDate;
+
+}
